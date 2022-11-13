3ab4e732d7a7bdf2d01239df87a969e2e5c281f6
Adam Moen <C19318671@mytudublin.ie>
2022-11-13 14:01:44 +0000

    Added Tunnel

--- Assets/Scripts/Spread.cs
+++ Assets/Scripts/Spread.cs
@@ -54,11 +54,6 @@ public class Spread : MonoBehaviour
         }
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
 
     // Update is called once per frame
     void Update()
---
+++ Assets/Scripts/Vortex.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Vortex : MonoBehaviour
+{
+    public Transform vortex;
+    public float vortexSpeed, cameraDistance;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        vortex.position = new Vector3(vortex.position.x, vortex.position.y, vortex.position.z + (Time.deltaTime * vortexSpeed));
+        this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, vortex.position.z + cameraDistance);
+    }
+}
+
+
