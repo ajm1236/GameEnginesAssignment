4f580005ee34374983cea62c5746b97eaca59ae8
Adam Moen <C19318671@mytudublin.ie>
2022-11-18 14:00:16 +0000

    Making Distance toggleable
    Allows changing of the distance the Tardis is from the camera

--- Assets/Scripts/Tardis.cs
+++ Assets/Scripts/Tardis.cs
@@ -7,12 +7,14 @@ public class Tardis : MonoBehaviour
 {
     [Range(0, 360)]
     public float rotSpeed;
+    public float distance = 20;
     public Transform doctor;
+
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(0, rotSpeed * Time.deltaTime, 0);
-        this.transform.position = Camera.main.transform.position + Camera.main.transform.forward * 18;
+        this.transform.position = Camera.main.transform.position + Camera.main.transform.forward * distance;
     }
 }
 
