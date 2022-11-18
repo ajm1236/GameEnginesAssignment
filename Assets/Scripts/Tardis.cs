8d2c464974534330a4e00036949fb23fe1e71494
Adam Moen <C19318671@mytudublin.ie>
2022-11-18 14:22:31 +0000

    Tardis Rotate added to multiple axis

--- Assets/Scripts/Tardis.cs
+++ Assets/Scripts/Tardis.cs
@@ -7,13 +7,14 @@ public class Tardis : MonoBehaviour
 {
     [Range(0, 360)]
     public float rotSpeed;
+    [Range(5, 100)]
     public float distance = 20;
     public Transform doctor;
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, rotSpeed * Time.deltaTime, 0);
+        transform.Rotate(rotSpeed * Time.deltaTime, rotSpeed * Time.deltaTime, -(rotSpeed * Time.deltaTime));
         this.transform.position = Camera.main.transform.position + Camera.main.transform.forward * distance;
     }
 }
