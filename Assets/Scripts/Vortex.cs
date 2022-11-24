7eea09432d3f05787ce4b0c017b9de3370396ac0
Adam Moen <C19318671@mytudublin.ie>
2022-11-24 18:24:53 +0000

    Debugging

--- Assets/Scripts/Vortex.cs
+++ Assets/Scripts/Vortex.cs
@@ -14,10 +14,6 @@ public class Vortex : MonoBehaviour
     {
         vortex.position = new Vector3(vortex.position.x, vortex.position.y, vortex.position.z + (Time.deltaTime * vortexSpeed));
         this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, vortex.position.z + cameraDistance);
-        if(Input.GetKey(KeyCode.A))
-        {
-            vortex.position = new Vector3(vortex.position.x + 0.5f, vortex.position.y + 0.5f);
-        }
     }
 }
 
