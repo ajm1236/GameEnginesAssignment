2132119776f5849412ace0d894ef7e62d82f957c
Adam Moen <C19318671@mytudublin.ie>
2022-12-15 15:45:09 +0000

    Additional Bits altered
    VR Build Complete

--- Assets/Scripts/Tardis.cs
+++ Assets/Scripts/Tardis.cs
@@ -8,7 +8,7 @@ public class Tardis : MonoBehaviour
     [Range(0, 360)]
     public float rotSpeed;
     [Range(25, 200)]
-    public float distance = 60;
+    public float distance = 40;
     public Transform doctor;
 
     // Update is called once per frame
